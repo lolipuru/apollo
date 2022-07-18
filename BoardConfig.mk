f33b9ca7e9334d4acbac30ab493983f021dd3187
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-07-18 18:48:35 +0700

    apollo: ricedroid bringup
    also drop common vendor because i've going to use decommonized vendor
    
    Signed-off-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,5 +206,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
-include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
 include vendor/xiaomi/apollon/BoardConfigVendor.mk
