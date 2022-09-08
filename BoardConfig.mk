eb7e85e89be492cfe5bdc5da59ce4647cb6d664a
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-09-09 03:17:14 +0700

    apollo: evolution bringup
    also use proton-clang
    
    Signed-off-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_CONFIG := apollo_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := apollo,apollopro
