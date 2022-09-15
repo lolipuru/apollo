431ab73fb1fa74fa0cd49c9d235ec91a66d7586f
johnmart19 <johnivan19999@gmail.com>
2022-09-15 03:11:08 +0300

    sm8250-common: Build libaudio-resampler
    - Matching stock Miui environment:
    system/lib64/libaudio-resampler.so
    system/lib/libaudio-resampler.so

--- device.mk
+++ device.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libaudiopreprocessing \
+    libaudio-resampler \
     libbatterylistener \
     libbundlewrapper \
     libcomprcapture \
