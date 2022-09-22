797db4ec59f7913cde11fe973e9d43c231d5c206
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-09-22 18:23:14 +0700

    apollo: use raven september fingerprint
    to fix play protect not certified
    
    Signed-off-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>

--- evolution_apollo.mk
+++ evolution_apollo.mk
@@ -27,6 +27,7 @@ PRODUCT_MODEL := Xiaomi Mi 10T
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="apollo_global-user 12 RKQ1.211001.001 V13.0.5.0.SJDMIXM release-keys"
+    PRODUCT_NAME="apollo" \
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
 
-BUILD_FINGERPRINT := Redmi/apollo_global/apollo:12/RKQ1.211001.001/V13.0.5.0.SJDMIXM:user/release-keys
+BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
