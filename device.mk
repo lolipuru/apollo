b50b6486f237cdefdd518f9be8d4026915207526
johnmart19 <johnivan19999@gmail.com>
2022-10-11 11:37:33 +0300

    sm8250-common: Dolby: Move Permissions to Device trees
    - Needed to be able to freely modify and update them if required

--- device.mk
+++ device.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/xiaomi/apollo/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
+# Dolby Permissions
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/permissions/,$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
